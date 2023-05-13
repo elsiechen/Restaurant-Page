3b6856f865fb7886af962cd9d9aea520f88d64bc
elsie <elsiechen915@gmail.com>
2023-05-12 23:15:06 -0400

    Install style-loader and css-loader and import css file within js module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+import './style.css';
+
+const content = document.querySelector('#content');
+
+const head = document.createElement('div');
+head.innerHTML = 'Taiwan Cafe';
+head.classList.add('head');
+
+content.appendChild(head);
+
 console.log('Restaurant Page');
 console.log('Go ahead!');
