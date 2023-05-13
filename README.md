07b23febf6174ecb7a6b86ca0fbb94ba340fc7ce
elsie <elsiechen915@gmail.com>
2023-05-12 23:57:17 -0400

    Add photo and configure module.rule

---
+++ README.md
@@ -0,0 +1,4 @@
+
+hot pot picture:
+vogue taiwan
+https://www.vogue.com.tw/lifestyle/article/%E5%8F%B0%E4%B8%AD-%E9%BA%BB%E8%BE%A345
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import './style.css';
+import Photo from './hotpot.png';
 
 const content = document.querySelector('#content');
 
 const head = document.createElement('div');
 head.innerHTML = 'Taiwan Cafe';
 head.classList.add('head');
-
 content.appendChild(head);
+
+const photo = document.createElement('img');
+photo.setAttribute('src', Photo);
+photo.setAttribute('width', '100%');
+photo.setAttribute('height', '100%');
+content.appendChild(photo);
 
 console.log('Restaurant Page');
 console.log('Go ahead!');
