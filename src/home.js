eec99e66562751fc678d6d8c039695770882974e
elsie <elsiechen915@gmail.com>
2023-05-15 11:24:30 -0400

    Experiment on appending element from home.js to index.js

---
+++ src/home.js
@@ -0,0 +1,8 @@
+
+const homeTab = () => {
+    const home = document.createElement('div');
+    home.innerHTML = 'Home content';
+    return { home };
+}
+
+export default homeTab;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import pageLoad from './page-load.js'
+import homeTab from './home.js';
 
 pageLoad();
+const HomeTab = homeTab();
+console.log(HomeTab.home);
+document.body.appendChild(HomeTab.home);
 
 console.log('Restaurant Page');
 console.log('Go ahead!');
