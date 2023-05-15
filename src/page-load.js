6eeb9aca8590286dbf22d99ae5faddd2a193b03a
elsie <elsiechen915@gmail.com>
2023-05-15 09:38:38 -0400

    Finish head container content

--- src/page-load.js
+++ src/page-load.js
@@ -8,29 +8,48 @@ export default function pageLoad(){
     const container = document.createElement('div');
     const headContainer = document.createElement('div');
     const logoContainer = document.createElement('div');
+    const navContainer = document.createElement('div');
 
     background.classList.add('backgroundImg');
     body.appendChild(background);
-
+    // headContainer > 
+    // ( logoContainer > logo + shopName )
+    // + ( navContainer > home + menu + contact )
     headContainer.classList.add('headContainer');
-    
-    console.log(Logo);
+    logoContainer.classList.add('logoContainer');
+    navContainer.classList.add('navContainer');
+    //  insert svg logo
     var logo = document.createElement('object');
     logo.setAttribute('type', 'image/svg+xml');
     logo.setAttribute('data', Logo);
-    logo.setAttribute('width', '120rem');
-    logo.setAttribute('height', '120rem');
+    logo.setAttribute('width', '80rem');
+    logo.setAttribute('height', '80rem');
     logo.classList.add('logo');
     logoContainer.appendChild(logo);
-
+    // shopName
     const shopName = document.createElement('div');
     shopName.innerHTML = 'Taiwan Cafe';
     shopName.classList.add('shopName');
     logoContainer.appendChild(shopName);
 
     headContainer.appendChild(logoContainer);
+    // home
+    const home = document.createElement('div');
+    home.innerHTML = 'Home';
+    home.classList.add('navLink');
+    navContainer.appendChild(home);
+    // menu
+    const menu = document.createElement('div');
+    menu.innerHTML = 'Menu';
+    menu.classList.add('navLink');
+    navContainer.appendChild(menu);
+    // contact
+    const contact = document.createElement('div');
+    contact.innerHTML = 'Contact';
+    contact.classList.add('navLink');
+    navContainer.appendChild(contact);
 
+    headContainer.appendChild(navContainer);
     container.appendChild(headContainer);
-
     body.appendChild(container);
 };
