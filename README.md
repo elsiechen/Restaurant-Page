998edbffc0a62c4d31d3437568dc7f054f77ec0c
elsie <elsiechen915@gmail.com>
2023-05-15 14:21:01 -0400

    Finish home tab

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 hot pot picture:
 vogue taiwan
 https://www.vogue.com.tw/lifestyle/article/%E5%8F%B0%E4%B8%AD-%E9%BA%BB%E8%BE%A345
+
+taiwan picture:
+pngtree
+https://pngtree.com/freepng/hand-painted-taiwanese-brain--micro-world-travel-box_4104075.html
--- src/home.js
+++ src/home.js
@@ -1,7 +1,23 @@
+import Taiwan from './taiwan.png'
 
 const homeTab = () => {
     const home = document.createElement('div');
-    home.innerHTML = 'Home content';
+    home.classList.add('homeContainer');
+    
+    const slogan1 = document.createElement('div');
+    slogan1.innerHTML = 'Experience the vibrant flavors of Taiwan at our cafe.';
+    slogan1.classList.add('slogan1');
+    home.appendChild(slogan1);
+
+    const taiwan = document.createElement('img');
+    taiwan.setAttribute('src', Taiwan);
+    taiwan.classList.add('taiwan');
+    home.appendChild(taiwan);
+
+    const slogan2 = document.createElement('div');
+    slogan2.innerHTML = 'Join us for a taste of energy and excitement!';
+    slogan2.classList.add('slogan2');
+    home.appendChild(slogan2);
 
     return { home };
 };
