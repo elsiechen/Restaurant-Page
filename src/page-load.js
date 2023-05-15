558624ed78769787d073c19f5f3e0ce185e654dd
elsie <elsiechen915@gmail.com>
2023-05-15 10:51:05 -0400

    Build content container and footer

--- src/page-load.js
+++ src/page-load.js
@@ -1,5 +1,6 @@
 import './style.css';
 import Logo from './logo.svg';
+import Github from './github.png';
 import { doc } from 'prettier';
 
 export default function pageLoad(){
@@ -9,9 +10,15 @@ export default function pageLoad(){
     const headContainer = document.createElement('div');
     const logoContainer = document.createElement('div');
     const navContainer = document.createElement('div');
+    const contentContainer = document.createElement('div');
+    const footer = document.createElement('div');
 
     background.classList.add('backgroundImg');
     body.appendChild(background);
+
+    // container 
+    container.classList.add('container');
+
     // headContainer > 
     // ( logoContainer > logo + shopName )
     // + ( navContainer > home + menu + contact )
@@ -19,7 +26,7 @@ export default function pageLoad(){
     logoContainer.classList.add('logoContainer');
     navContainer.classList.add('navContainer');
     //  insert svg logo
-    var logo = document.createElement('object');
+    const logo = document.createElement('object');
     logo.setAttribute('type', 'image/svg+xml');
     logo.setAttribute('data', Logo);
     logo.setAttribute('width', '80rem');
@@ -49,7 +56,28 @@ export default function pageLoad(){
     contact.classList.add('navLink');
     navContainer.appendChild(contact);
 
+    // content container
+    contentContainer.classList.add('contentContainer');
+
+    // footer > footerContent + ( link > github )
+    footer.classList.add('footer');
+    const footerContent = document.createElement('div');
+    footerContent.innerHTML = 'Copyrignt &#169; 2023 Yi-Chun Chen';
+    footer.appendChild(footerContent);
+
+    const link = document.createElement('a');
+    link.setAttribute('href', 'https://github.com/elsiechen');
+
+    const github = document.createElement('img');
+    github.setAttribute('src', Github);
+    github.classList.add('github');
+    link.appendChild(github);
+    footer.appendChild(link);
+
     headContainer.appendChild(navContainer);
     container.appendChild(headContainer);
+    container.appendChild(contentContainer);
+    container.appendChild(footer);
+
     body.appendChild(container);
 };
